0f867eb8107d1d2a0679d6952ebe03416cc93bca
palingheorghe <gheorghe.palin@gmail.com>
2021-03-08 14:18:49 +0200

    v1.4.0

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
 import { Block, GalioProvider } from 'galio-framework';
