bc3ee16d990dc7e472aa1c146a9c8c5a3d31e51d
symphony-dev <abdul.rehman@symphonylogics.com>
2022-04-09 17:45:48 +0500

    shahid updates

--- components/DrawerItem.js
+++ components/DrawerItem.js
@@ -1,102 +1,102 @@
-import React from "react";
-import { StyleSheet, TouchableOpacity, Linking } from "react-native";
-import { Block, Text, theme } from "galio-framework";
+import { Block, Text, theme } from 'galio-framework';
+import { Linking, StyleSheet, TouchableOpacity } from 'react-native';
 
-import Icon from "./Icon";
-import nowTheme from "../constants/Theme";
+import Icon from './Icon';
+import React from 'react';
+import nowTheme from '../constants/Theme';
 
 class DrawerItem extends React.Component {
   renderIcon = () => {
     const { title, focused } = this.props;
 
     switch (title) {
-      case "Home":
+      case 'Home':
         return (
           <Icon
             name="app2x"
             family="NowExtra"
             size={18}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             style={{ opacity: 0.5 }}
           />
         );
-      case "Components":
+      case 'Components':
         return (
           <Icon
             name="atom2x"
             family="NowExtra"
             size={18}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             style={{ opacity: 0.5 }}
           />
         );
-      case "Articles":
+      case 'Articles':
         return (
           <Icon
             name="paper"
             family="NowExtra"
             size={18}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             style={{ opacity: 0.5 }}
           />
         );
-      case "Profile":
+      case 'Profile':
         return (
           <Icon
             name="profile-circle"
             family="NowExtra"
             size={18}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             style={{ opacity: 0.5 }}
           />
         );
-      case "Account":
+      case 'Account':
         return (
           <Icon
             name="badge2x"
             family="NowExtra"
             size={18}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             style={{ opacity: 0.5 }}
           />
         );
-      case "Settings":
+      case 'Settings':
         return (
           <Icon
             name="settings-gear-642x"
             family="NowExtra"
             size={18}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             style={{ opacity: 0.5 }}
           />
         );
-      case "Examples":
+      case 'Examples':
         return (
           <Icon
             name="album"
             family="NowExtra"
             size={14}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
           />
         );
-      case "GETTING STARTED":
+      case 'GETTING STARTED':
         return (
           <Icon
             name="spaceship2x"
             family="NowExtra"
             size={18}
-            style={{ borderColor: "rgba(0,0,0,0.5)", opacity: 0.5 }}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            style={{ borderColor: 'rgba(0,0,0,0.5)', opacity: 0.5 }}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
           />
         );
-      case "LOGOUT":
+      case 'LOGOUT':
         return (
           <Icon
             name="share"
             family="NowExtra"
             size={18}
-            style={{ borderColor: "rgba(0,0,0,0.5)", opacity: 0.5 }}
-            color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+            style={{ borderColor: 'rgba(0,0,0,0.5)', opacity: 0.5 }}
+            color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
           />
         );
       default:
@@ -109,17 +109,17 @@ class DrawerItem extends React.Component {
 
     const containerStyles = [
       styles.defaultStyle,
-      focused ? [styles.activeStyle, styles.shadow] : null
+      focused ? [styles.activeStyle, styles.shadow] : null,
     ];
 
     return (
       <TouchableOpacity
         style={{ height: 60 }}
         onPress={() =>
-          title == "GETTING STARTED"
-            ? Linking.openURL(
-                "https://demos.creative-tim.com/now-ui-pro-react-native/docs/"
-              ).catch(err => console.error("An error occurred", err))
+          title == 'GETTING STARTED'
+            ? Linking.openURL('https://demos.creative-tim.com/now-ui-pro-react-native/docs/').catch(
+                (err) => console.error('An error occurred', err)
+              )
             : navigation.navigate(title == 'LOGOUT' ? 'Onboarding' : title)
         }
       >
@@ -130,13 +130,13 @@ class DrawerItem extends React.Component {
           <Block row center flex={0.9}>
             <Text
               style={{
-                fontFamily: "montserrat-regular",
-                textTransform: "uppercase",
-                fontWeight: "300"
+                fontFamily: 'montserrat-regular',
+                textTransform: 'uppercase',
+                fontWeight: '300',
               }}
               size={12}
               bold={focused ? true : false}
-              color={focused ? nowTheme.COLORS.PRIMARY : "white"}
+              color={focused ? nowTheme.COLORS.PRIMARY : 'black'}
             >
               {title}
             </Text>
@@ -151,22 +151,22 @@ const styles = StyleSheet.create({
   defaultStyle: {
     paddingVertical: 15,
     paddingHorizontal: 14,
-    color: "white"
+    color: 'white',
   },
   activeStyle: {
     backgroundColor: nowTheme.COLORS.WHITE,
     borderRadius: 30,
-    color: "white"
+    color: 'white',
   },
   shadow: {
     shadowColor: theme.COLORS.BLACK,
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowRadius: 8,
-    shadowOpacity: 0.1
-  }
+    shadowOpacity: 0.1,
+  },
 });
 
 export default DrawerItem;
--- navigation/Menu.js
+++ navigation/Menu.js
@@ -1,51 +1,23 @@
-import React from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Dimensions,
-  Image,
-  TouchableOpacity,
-  Linking
-} from "react-native";
-import { Block, Text, theme } from "galio-framework";
-import { useSafeArea } from "react-native-safe-area-context";
-import Images from "../constants/Images";
-import { DrawerItem as DrawerCustomItem, Icon } from "../components";
+import { Block, Text, theme } from 'galio-framework';
+import { Dimensions, Image, Linking, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { DrawerItem as DrawerCustomItem, Icon } from '../components';
 
-import nowTheme from "../constants/Theme";
+import Images from '../constants/Images';
+import React from 'react';
+import nowTheme from '../constants/Theme';
+import { useSafeArea } from 'react-native-safe-area-context';
 
-const { width } = Dimensions.get("screen");
+const { width } = Dimensions.get('screen');
 
-function CustomDrawerContent({
-  drawerPosition,
-  navigation,
-  profile,
-  focused,
-  state,
-  ...rest
-}) {
+function CustomDrawerContent({ drawerPosition, navigation, profile, focused, state, ...rest }) {
   const insets = useSafeArea();
-  const screens = [
-    "Home",
-    "Components",
-    "Articles",
-    "Profile",
-    "Account",
-  ];
+  const screens = ['Home', 'Components', 'Articles', 'Profile', 'Account'];
   return (
-    <Block
-      style={styles.container}
-      forceInset={{ top: "always", horizontal: "never" }}
-    >
+    <Block style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
       <Block style={styles.header}>
         <Image style={styles.logo} source={Images.Logo} />
         <Block right style={styles.headerIcon}>
-          <Icon
-            name="align-left-22x"
-            family="NowExtra"
-            size={15}
-            color={"white"}
-          />
+          <Icon name="align-left-22x" family="NowExtra" size={15} color={'black'} />
         </Block>
       </Block>
       <Block flex style={{ paddingLeft: 8, paddingRight: 14 }}>
@@ -61,18 +33,30 @@ function CustomDrawerContent({
             );
           })}
           <Block flex style={{ marginTop: 24, marginVertical: 8, paddingHorizontal: 8 }}>
-          <Block
-            style={{ borderColor: 'white', width: '93%', borderWidth: StyleSheet.hairlineWidth, marginHorizontal: 10}}
-          />
-          <Text
-            color={nowTheme.COLORS.WHITE}
-            style={{ marginTop: 30, marginLeft: 20, marginBottom: 10, fontFamily: 'montserrat-regular', fontWeight: '300', fontSize: 12}}
-          >
-            DOCUMENTATION
-          </Text>
-        </Block>
-        <DrawerCustomItem title="GETTING STARTED" navigation={navigation}/>
-        <DrawerCustomItem title="LOGOUT" navigation={navigation}/>
+            <Block
+              style={{
+                borderColor: 'black',
+                width: '93%',
+                borderWidth: StyleSheet.hairlineWidth,
+                marginHorizontal: 10,
+              }}
+            />
+            <Text
+              color={nowTheme.COLORS.BLACK}
+              style={{
+                marginTop: 30,
+                marginLeft: 20,
+                marginBottom: 10,
+                fontFamily: 'montserrat-regular',
+                fontWeight: '300',
+                fontSize: 12,
+              }}
+            >
+              DOCUMENTATION
+            </Text>
+          </Block>
+          <DrawerCustomItem title="GETTING STARTED" navigation={navigation} />
+          <DrawerCustomItem title="LOGOUT" navigation={navigation} />
         </ScrollView>
       </Block>
     </Block>
@@ -81,21 +65,22 @@ function CustomDrawerContent({
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   header: {
     paddingHorizontal: 28,
     paddingBottom: theme.SIZES.BASE,
     paddingTop: theme.SIZES.BASE * 3,
-    justifyContent: "center"
+    justifyContent: 'center',
   },
   headerIcon: {
-    marginTop: -20
+    marginTop: -20,
   },
   logo: {
     height: 40,
-    width: 37
-  }
+    width: 37,
+    tintColor: 'black',
+  },
 });
 
 export default CustomDrawerContent;
